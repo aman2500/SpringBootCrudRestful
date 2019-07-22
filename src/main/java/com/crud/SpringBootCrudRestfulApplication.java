859719144a157119df65b87c068fbe0f12706493
aman2500 <apatel2500@gmail.com>
2019-07-22 16:34:57 -0500

    Initial commit

---
+++ src/main/java/com/crud/SpringBootCrudRestfulApplication.java
@@ -0,0 +1,14 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudRestfulApplication.class, args);
+		System.out.println("done with main");
+	}
+
+}
